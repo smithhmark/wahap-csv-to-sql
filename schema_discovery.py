d2a2c38bfeb40b6d51d180693ee4fa44fe332859
Mark Smith <smith.h.mark@gmail.com>
2020-12-23 17:33:32 -0500

    column type overrides

--- schema_discovery.py
+++ schema_discovery.py
@@ -1,6 +1,10 @@
 import excel
 
-
+DEFAULT_TYPE_OVERRIDES = {
+        "str": "text",
+        "string": "text",
+        "num": "int",
+        }
 
 def column_generator(column_spec):
     column_name = column_spec['name']
@@ -11,3 +15,15 @@ def column_generator(column_spec):
 def table_generator(table_spec):
     cols = [column_generator(spec) for spec in table_spec['columns']]
     return f"""CREATE TABLE {table_spec['name']} ( {', '.join(cols)} ) ;"""
+
+def _transform_column(raw, type_overrides):
+    output = {}
+    output['name'] = raw['field']
+    output['type'] = type_overrides.get(raw['type'], raw['type'])
+    return output
+
+def to_spec(table_name, columns, type_overrides=DEFAULT_TYPE_OVERRIDES):
+    spec = {"name": table_name}
+    cols = [ _transform_column(dd, type_overrides) for dd in columns]
+    spec['columns'] = cols
+    return spec
--- schema_discovery_test.py
+++ schema_discovery_test.py
@@ -3,17 +3,17 @@ import pytest
 import schema_discovery as sd
 
 @pytest.fixture
-def ex_column_spec():
+def toy_column_spec():
     return { "name": "col1",
                     "type": "text",
                     }
 
 
 @pytest.fixture
-def ex_table_spec(ex_column_spec):
+def toy_table_spec(toy_column_spec):
     return {"name": "my table",
             "columns": [
-                ex_column_spec,
+                toy_column_spec,
                 {
                     "name": "column two",
                     "type": "int",
@@ -21,19 +21,52 @@ def ex_table_spec(ex_column_spec):
                 ]
             }
 
+@pytest.fixture
+def ex_table():
+    return [
+            {"field": "id", "type": "string"},
+            {"field": "name", "type": "string"},
+            {"field": "cost", "type": "int"},
+            {"field": "date", "type": "date"},
+        ]
+
+@pytest.fixture
+def ex_spec_bare():
+    return {
+            "name": "example_table",
+            "columns": [
+                {"name": "id", "type": "text"},
+                {"name": "name", "type": "text"},
+                {"name": "cost", "type": "int"},
+                {"name": "date", "type": "date"},
+                ],
+            }
+
+def test_transform_column():
+    given = {"field": "id", "type": "string"}
+    expect = {"name": "id", "type": "text"}
+    rcvd = sd._transform_column(given, {"string": "text"})
+    assert rcvd == expect
+
+def test_table_to_spec(ex_table, ex_spec_bare):
+    rcvd = sd.to_spec("example_table", ex_table)
+    print(rcvd)
+    print(ex_spec_bare)
+    assert rcvd == ex_spec_bare
+
 @pytest.mark.xfail
-def test_table_to_schema():
+def test_table_to_sql(ex_table):
     pass
 
 @pytest.mark.xfail
 def test_override_type():
     pass
 
-def test_column_generator(ex_column_spec):
-    expected = f'''{ex_column_spec['name']} {ex_column_spec['type']}'''
-    assert sd.column_generator(ex_column_spec) == expected
+def test_column_generator(toy_column_spec):
+    expected = f'''{toy_column_spec['name']} {toy_column_spec['type']}'''
+    assert sd.column_generator(toy_column_spec) == expected
 
-def test_table_generator(ex_table_spec):
-    expect = f"""CREATE TABLE {ex_table_spec['name']} ( {ex_table_spec['columns'][0]['name']} {ex_table_spec['columns'][0]['type']}, {ex_table_spec['columns'][1]['name']} {ex_table_spec['columns'][1]['type']} ) ;"""
+def test_table_generator(toy_table_spec):
+    expect = f"""CREATE TABLE {toy_table_spec['name']} ( {toy_table_spec['columns'][0]['name']} {toy_table_spec['columns'][0]['type']}, {toy_table_spec['columns'][1]['name']} {toy_table_spec['columns'][1]['type']} ) ;"""
 
-    assert sd.table_generator(ex_table_spec) == expect
+    assert sd.table_generator(toy_table_spec) == expect
