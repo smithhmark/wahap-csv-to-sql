c9d16f0da8182fc4f4fd0760db35a7e59c641b93
Mark Smith <smith.h.mark@gmail.com>
2020-12-23 16:52:41 -0500

    more sql hacking

--- schema_discovery.py
+++ schema_discovery.py
@@ -6,3 +6,8 @@ def column_generator(column_spec):
     column_name = column_spec['name']
     column_type = column_spec['type']
     return f'''{column_name} {column_type}'''
+
+
+def table_generator(table_spec):
+    cols = [column_generator(spec) for spec in table_spec['columns']]
+    return f"""CREATE TABLE {table_spec['name']} ( {', '.join(cols)} ) ;"""
--- schema_discovery_test.py
+++ schema_discovery_test.py
@@ -10,10 +10,10 @@ def ex_column_spec():
 
 
 @pytest.fixture
-def example_table_spec(ex_column_spec):
+def ex_table_spec(ex_column_spec):
     return {"name": "my table",
             "columns": [
-                example_table_spec,
+                ex_column_spec,
                 {
                     "name": "column two",
                     "type": "int",
@@ -33,7 +33,7 @@ def test_column_generator(ex_column_spec):
     expected = f'''{ex_column_spec['name']} {ex_column_spec['type']}'''
     assert sd.column_generator(ex_column_spec) == expected
 
-@pytest.mark.xfail
-def test_table_generator(example_table_spec):
-    pass
+def test_table_generator(ex_table_spec):
+    expect = f"""CREATE TABLE {ex_table_spec['name']} ( {ex_table_spec['columns'][0]['name']} {ex_table_spec['columns'][0]['type']}, {ex_table_spec['columns'][1]['name']} {ex_table_spec['columns'][1]['type']} ) ;"""
 
+    assert sd.table_generator(ex_table_spec) == expect
